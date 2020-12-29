1d3fc15d43a44031155e28599895e5849c776864
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-29 16:54:17 -0300

    Implementando unform

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -12,6 +12,7 @@ export const Container = styled.button`
     font-weight: 500;
     margin-top: 16px;
     transition: background-color 0.2;
+    outline: none;
 
     &:hover {
         background: ${shade(0.2, '#ff9000')};
