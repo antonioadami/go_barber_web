3286b75b493fa35cad5e029955d82a4ae56bfc4e
Antonio Adami <antonioadamipalma@gmail.com>
2020-12-30 13:53:37 -0300

    Hook useAuth

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import * as Yup from 'yup';
 import { Form } from '@unform/web';
@@ -11,7 +11,7 @@ import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 import getValidationErrors from '../../utils/getValidationErrors';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 interface SignInFormData {
     email: string;
@@ -21,9 +21,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
-    const { user, signIn } = useContext(AuthContext);
-
-    console.log(user);
+    const { signIn } = useAuth();
 
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
